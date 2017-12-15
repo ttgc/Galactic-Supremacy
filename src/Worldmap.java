5a2c948d018d04f70087c0eef38bc697a6a8a42b
ttgc <ttgc@users.noreply.github.com>
2017-12-15 16:06:15 +0100

    adding licences to files

--- src/Worldmap.java
+++ src/Worldmap.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+	Galactic Supremacy, Shoot'em up game
+	Copyright (C) 2017, 2018  PIOT Thomas
+		
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+		
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -25,10 +43,11 @@ public class Worldmap extends BasicGameState {
 		//définition des niveaux
 		map.add_level(600, 508);
 		map.add_level(508, 418);
-		map.add_level(416, 316);
+		map.add_level(200, 316);
 		map.add_level(308, 236);
 		map.add_level(308, 188);
-		map.add_level(200, 108);
+		map.add_level(416, 108);
+		Game.player.setLevel(3);
 		ship = new Image("Pictures/ship.png");
 		game= sbg;
 
@@ -76,7 +95,6 @@ public class Worldmap extends BasicGameState {
 			}
 			while (diff > 0) {
 				map.enable_next();
-				//map.add_level(xlev[map.getAvalaible_level()-1], ylev[map.getAvalaible_level()-1]);
 				diff--;
 			}
 		}
